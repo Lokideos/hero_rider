dc8cc3cf72a2684c0c123949aaa2c89f61c917bd
lokideos <archmage09@gmail.com>
2020-07-27 12:00:03 +0300

    Initial chat commit

---
+++ chat/config/application_loader.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module ApplicationLoader
+  extend self
+
+  def load_app!
+    init_config
+    require_app
+    init_app
+  end
+
+  def root
+    File.expand_path('..', __dir__)
+  end
+
+  private
+
+  def init_config
+    require_file 'config/initializers/config'
+  end
+
+  def init_app
+    require_dir 'config/initializers'
+  end
+
+  def require_app
+    require_file 'config/application'
+    require_file 'app/service/base'
+    require_dir 'app/contracts'
+    require_dir 'app'
+  end
+
+  def require_file(path)
+    require File.join(root, path)
+  end
+
+  def require_dir(path)
+    path = File.join(root, path)
+    Dir["#{path}/**/*.rb"].sort.each { |file| require file }
+  end
+end
