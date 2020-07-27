2077d40b6d4008b8cc6f4059c2608164d5675bb2
lokideos <archmage09@gmail.com>
2020-07-27 18:40:08 +0300

    Add default route

--- data/config/application.rb
+++ data/config/application.rb
@@ -8,4 +8,22 @@ class Application < Roda
   plugin(:not_found) { { error: 'Not found' } }
   plugin :environments
   plugin :json_parser
+
+  route do |r|
+    r.root do
+      response['Content-Type'] = 'application/json'
+      response.status = 200
+      { status: 'ok' }.to_json
+    end
+
+    r.get 'favicon.ico' do
+      'no icon'
+    end
+  end
+
+  private
+
+  def params
+    request.params
+  end
 end
