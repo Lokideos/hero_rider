5e7c0899827e8211705fdfb923600d93bf7d4080
lokideos <archmage09@gmail.com>
2020-09-20 20:14:12 +0300

    Adjust line legnth in game trophy top for small smartphones

--- app/services/chat/game_top_service.rb
+++ app/services/chat/game_top_service.rb
@@ -4,7 +4,7 @@ module Chat
   class GameTopService
     prepend BasicService
 
-    MAX_NAME_LENGTH = 15
+    MAX_NAME_LENGTH = 13
     MAX_PROGRESS_LENGTH = 4
     PLATINUM_EARN_ICON = "\xF0\x9F\x8F\x86"
     FIRST_PLACE_ICON = "\xF0\x9F\xA5\x87"
@@ -28,7 +28,7 @@ module Chat
         platinum_speed = progress['platinum_speed']
         placement = platinum_placement.present? ? platinum_placement_icon(platinum_placement) : nil
         speed = platinum_speed.present? ? platinum_speed_icon(platinum_speed) : nil
-        player_name = player_name[0..12] + '..' if player_name.length > 13
+        player_name = player_name[0..10] + '..' if player_name.length > 11
         game_top << "<code>#{player_name} " + ' ' * (MAX_NAME_LENGTH - player_name.length) +
                     player_progress.to_s +
                     ' ' * (MAX_PROGRESS_LENGTH - player_progress.to_s.length) + '</code>' +
