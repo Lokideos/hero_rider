2b73a4a8c5cb785497385a18440d4c3cfacd4c55
lokideos <archmage09@gmail.com>
2021-04-11 16:56:43 +0300

    Create separate abstractino for cache storage

--- app/services/watcher/notify_forbidden_status_service.rb
+++ app/services/watcher/notify_forbidden_status_service.rb
@@ -7,17 +7,18 @@ module Watcher
     NOTIFY_TIME_INTERVAL = 86_400
 
     param :player_name
-    option :access_key, default: proc { "holy_rider:watcher:notification_interval:forbidden_request:#{@player_name}" }
+    option :access_key, default: proc { "watcher:notification_interval:forbidden_request:#{@player_name}" }
     option :chat_id, default: proc { Settings.telegram.admin_chat_id }
+    option :storage, default: proc { Storage::Cache.new }
 
     def call
       return unless Player.find(trophy_account: @player_name).present?
 
-      return if RedisDb.redis.exists?(@access_key)
+      return if @storage.exists?(key: @access_key)
 
       message = I18n.t('watcher.trophy_access_forbidden', player_name: player)
       Chat::SendChatMessageService.new(message, false, @chat_id).call
-      RedisDb.redis.setex(@access_key, NOTIFY_TIME_INTERVAL, 'notification_sent')
+      @storage.store_for_period(key: @access_key, period: NOTIFY_TIME_INTERVAL, value: 'notification_sent')
     end
   end
 end
