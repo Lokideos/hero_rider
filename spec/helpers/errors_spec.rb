d2dc3fbd160cca91597c336dad05a453a32a3c28
lokideos <archmage09@gmail.com>
2021-08-05 23:19:43 +0300

    179138096 | Add specs for errors helper

---
+++ spec/helpers/errors_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe Errors, type: :helper do
+  subject(:instance_with_errors) { klass.new }
+
+  let(:klass) do
+    Class.new do
+      include Errors
+
+      attr_reader :response
+
+      def initialize
+        @response = Rack::MockResponse.new(404, {}, {})
+      end
+    end
+  end
+
+  describe '#not_found_response' do
+    it 'updates response status to 200' do
+      instance_with_errors.not_found_response
+
+      expect(instance_with_errors.response.status).to eq(200)
+    end
+
+    it 'set response content-type to application/json' do
+      instance_with_errors.not_found_response
+
+      expect(instance_with_errors.response['Content-Type']).to eq('application/json')
+    end
+
+    it 'returns correct JSON' do
+      correct_json = { status: 'Page not found' }.to_json
+
+      expect(instance_with_errors.not_found_response).to eq(correct_json)
+    end
+  end
+end
