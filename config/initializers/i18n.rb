70a7ffa37ffb1e1afcefc1b128ec8db8f7d4ad42
lokideos <archmage@hey.com>
2022-09-29 16:50:24 +0600

    [177137046] Add fallback for ru locale

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'i18n/backend/fallbacks'
+I18n::Backend::Simple.include I18n::Backend::Fallbacks
+
 I18n.load_path += Dir[ApplicationLoader.root.concat('/config/locales/**/*.yml')]
 I18n.available_locales = %i[en ru]
 I18n.default_locale = :ru
+I18n.fallbacks[:ru] = %i[ru en]
